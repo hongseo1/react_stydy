b14584034d7e4ec6adc7a94860fc3f31e07613e6
hongseo1 <hongseow109@gmail.com>
2025-09-22 17:27:22 +0900

    react 강의 ex05 (state, useState)

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ function App() {
   return (
     <div className='App'>
       <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 }
 
 export default App
+
+/* 
+  동일한 컴포넌트여도 state는 각각 관리되며 다른 state에 영향을 주기 않는다.
+*/
--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,44 +1,22 @@
+import { useState } from "react";
+
 export default function Hello(){
-    /* 1 */
-    function showName(){
-        console.log("Hong");
+    // let name = "Hong"
+    const [name, setName] = useState("Hong");
+    // = 배열 구조분해 방식 useState는 배열을 반환한다. 배열의 첫번 째 값은 변수명(state), 두번 째 값은 state를 변경해주는 함수명 / useState("Hong"); = 초기값
+
+    function changeName(){
+        const newName = name === "Hong" ? "Jene" : "Hong"; 
+        setName(newName)
+        // name = name === "Hong" ? "Jene" : "Hong";
+        // console.log(name);
+        // document.getElementById("name").innerText = name;
     }
-    /* 2 */
-    function showAge(age){
-        console.log(age)
-    }
-    /* 3-1 function showText(e){
-        console.log(e.target.value);
-    } */
-    function showText(txt){ /* 3-3 */
-        console.log(txt);
-    }
+
     return (
         <div>
-            <h1>Hello</h1>
-            {/* 1 */}<button onClick={showName}>show name</button>
-            {/* 2 */}<button onClick={()=>{
-                showAge(25);
-            }}>show age</button>
-            {/* 3-1 <input type="text" onChange={showText}/> 아래와 동일 */}
-            {/*3-2 <input type="text" onChange={(e)=>{
-                console.log(e.target.value);
-            }} /> */}
-            {/* 3-3 */}
-            <input type="text" onChange={e =>{
-                const txt = e.target.value;
-                showText(txt); 
-            }} />
+            <h2 id="name">{name}</h2>
+            <button onClick={changeName}>Change</button>
         </div>
     );
 }
-
-/* 
-    <button onClick={()=>{
-        console.log(25);
-    }}>
-
-    이런 식으로 작성할 경우 매게변수를 전달하기 편하다는 장점이 있다.
-
-    이벤트 처리를 하는 방식은 여러 개니깐 상황에 맞게 가독성이 좋은 코드를 선택해서 작성하면 된다.
-*/
