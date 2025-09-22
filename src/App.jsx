e1bda902ab587a9c8dff2294e39f9fd3220485de
hongseo1 <hongseow109@gmail.com>
2025-09-22 21:54:40 +0900

    react 강의 ex06 (props)

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ function App() {
   
   return (
     <div className='App'>
-      <Hello />
-      <Hello />
-      <Hello />
+      <h3>props: properties</h3>
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
     </div>
   );
 }
@@ -15,5 +16,7 @@ function App() {
 export default App
 
 /* 
-  동일한 컴포넌트여도 state는 각각 관리되며 다른 state에 영향을 주기 않는다.
+  console에서 10 10 20 20 30 30 이런 식으로 두번씩 출력되는데 이는 리액트 개발모드에서 컴포넌트가 두 번 렌더링 되기 때문이다.
+  리액트는 개발 단계에서 잠재적인 문제를 미리 발견하고 경고하기 위해 <React.StrictMode>를 사용하는데 이 모드는 개발 환경에서 의도적으로 특정 동작을 두 번씩 실행시켜 부작용을 감지하도록 돕는다.
+  main.jsx 파일에서 <React.StrictMode>를 제거하면 두 번 출력되는 것을 해결할 수는 있느나 이는 중요한 도구이므로 권장하지 않는다.
 */
--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello(){
-    // let name = "Hong"
+/*export default function Hello(props){ 
+    console.log(props);
+    //props.age = 100; 
+    //에러 발생. props로 넘겨 받은 값은 컴포넌트에서 변경할 수 없다. 만약 값을 변경하고 싶다면 state를 다시 만들어야 한다. (props.age의 값을 변경하는 것이 아닌 그 값을 받은 새로운 객체를 생성하는 것이다.)
+
+    const [age, setAge] = useState(props.age); // 아래 버튼 클릭 시 age+1 처럼 받아온 props.age 값을 수정하기 위해서 정의. 위에서 설명한 것처럼 실제 props.age의 값이 변경되는 것은 아님. 받아 온 값을 새로운 객체에 담아서 수정하는 것. 
+
     const [name, setName] = useState("Hong");
-    // = 배열 구조분해 방식 useState는 배열을 반환한다. 배열의 첫번 째 값은 변수명(state), 두번 째 값은 state를 변경해주는 함수명 / useState("Hong"); = 초기값
-
-    function changeName(){
-        const newName = name === "Hong" ? "Jene" : "Hong"; 
-        setName(newName)
-        // name = name === "Hong" ? "Jene" : "Hong";
-        // console.log(name);
-        // document.getElementById("name").innerText = name;
-    }
 
     return (
         <div>
-            <h2 id="name">{name}</h2>
-            <button onClick={changeName}>Change</button>
+            <h2 id="name">{name}({age})</h2> //{({props.age}) }
+            <button onClick={()=>{
+                setName(name==="Hong" ? "Jane" : "Hong");
+                setAge(age + 1); App.jsx에서 각각 버튼 클릭 시 다르게 적용되는 것 확인 
+            }}>Change</button>
+        </div>
+    );
+*/
+
+//현재는 age 값만 받아오기 때문에 수정.
+export default function Hello({age}){ //{age} > 구주 분해 할당 문법
+    const [name, setName] = useState("Hong");
+    const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
+    
+    return (
+        <div>
+            <h2 id="name">{name}({age}) : {msg}</h2>
+            <UserName name={name} />
+            <button onClick={()=>{
+                setName(name==="Hong" ? "Jane" : "Hong");
+            }}>Change</button>
         </div>
     );
 }
+
+/*
+    화면의 데이터를 갱신하기 위해서는 state와 props 이 두가지를 사용해서 처리하는 것이 좋다.
+
+    props는 부모 컴포넌트에서 값을 넘길 때 사용하는 속성이지만,
+    한 컴퍼넌트가 가지고 있는 state를 props로 넘길 수도 있다.
+
+    예를 들어 Hello 컴포넌트에서는 name이 stete이지만 UserName 컴포넌트에서는 props 이다.
+*/
