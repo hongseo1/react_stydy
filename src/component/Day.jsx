c56cad9ad30e1636afd8c9cdb185bb44e6c4671b
hongseo1 <hongseow109@gmail.com>
2025-09-23 16:41:53 +0900

    react 강의ex10-2 (useEffect, fetch()로 API 호출하기 /  Day, DayList 컴포넌트 dummy 데이터 json-server로 변경 완료)

--- src/component/Day.jsx
+++ src/component/Day.jsx
@@ -1,22 +1,27 @@
-import dummy from '../db/data.json';
 import { useParams } from 'react-router-dom';
 import Word from './Word';
+import { useEffect, useState } from 'react';
 
 export default function Day(){
-    //dummy.words
-    // const day = 1;
-    // const a = useParams();
-    // console.log(a);
     const {day} = useParams();
-    const wordList = dummy.words.filter(word => word.day === Number(day)); //useParams에 day는 문자열이기 때문에 숫자형으로 바꿔 줘야 성립이된다.
-    console.log(wordList);
+    const [words, setWords] = useState([]);
+
+    useEffect(()=>{
+       fetch(`http://localhost:3001/words?day=${day}`)
+       .then(res=>{
+            return res.json();
+       })
+       .then(data=>{
+            setWords(data);
+       });
+    }, [day]);
 
     return ( 
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word => (
+                    {words.map(word => (
                         <Word word={word} key={word.id}/>
                     ))}
                 </tbody>
