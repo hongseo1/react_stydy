92d21f5eb08261da80cb5eb08db1642297b2c9d0
hongseo1 <hongseow109@gmail.com>
2025-09-23 11:30:59 +0900

    react 강의 ex08 (영단어 만들어 보기 (react-router-dom/리액트 라우터 사용 방법 v5&v6))

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
+//예제에서는 Switch를 사용했지만 v6 이상 버전에서는 Switch 컴포넌트가 제거 되었기 때문에 오류 발생, Switch 대신 Routes 사용. Routes 안에는 Route 컴포넌트만 자식으로 가질 수 있으므로 이동할 컴포넌트를 감싸 경로 지정을 해줘야한다. 
+//<Route path="/" element={<DayList />} />와 같이 <Routes> 내부에 <Route> 컴포넌트만 사용하고, element prop을 통해 DayList 컴포넌트를 렌더링해야 한다.
 
 function App() {
   
   return (
-    <div className='App'>
-      <Header/>
-      <DayList/>
-      <Day/>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<DayList/>}></Route>
+          <Route path="/day/:day" element={<Day/>}></Route>
+          <Route path="*" element={<EmptyPage/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App
+
+/* 
+<Switch>
+  <Route exact path="/">
+    <DayList/>
+  </Route>
+  <Route path="/day:day">
+    <Day/>
+  </Route>
+  <Route>
+    <EmptyPage/>
+  </Route>
+</Switch>
+Switch 사용해서 Route 작성 시 EmptyPage 컴포넌트가 하단에 있어야 한다. 
+<Switch>는 일치하는 첫 번째 <Route>만 렌더링하고, 이후 라우트들은 무시하기 때문이다.
+그렇기에 예외 처리용 라우트는 항상 마지막에 배치해야하는 것이다.
+
+하지만 v6 이상 번전에서 사용되는 <Routes>는 라우트 순서에 관계 없이 가장 정확하게 일치하는 경로를 찾아 렌더링한다. 경로가 구체적일 수록 /*와 같이 포괄적인 경로보다 우선순위가 높아진다. 그렇기에 예외 처리용 라우트 위치가 어디든 상관 없이 정확하게 동작한다는 차이가 있다.
+*/
--- src/component/Day.jsx
+++ src/component/Day.jsx
@@ -1,13 +1,18 @@
 import dummy from '../db/data.json';
+import { useParams } from 'react-router-dom';
 
 export default function Day(){
     //dummy.words
-    const day = 1;
-    const wordList = dummy.words.filter(word =>(word.day === day));
+    // const day = 1;
+    // const a = useParams();
+    // console.log(a);
+    const {day} = useParams();
+    const wordList = dummy.words.filter(word => word.day === Number(day)); //useParams에 day는 문자열이기 때문에 숫자형으로 바꿔 줘야 성립이된다.
     console.log(wordList);
 
     return ( 
         <>
+            <h2>Day {day}</h2>
             <table>
                 <tbody>
                     {wordList.map(word => (
