f97b6917aaf6348acc1689386b39226ed344a91c
hongseo1 <hongseow109@gmail.com>
2025-09-23 17:43:18 +0900

    react 강의 ex12 (PUT, DELETE 기능 구현)

--- src/component/Word.jsx
+++ src/component/Word.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Word({word}){
+export default function Word({word: w}){
+    const [word, setWord] = useState(w);
     const [isShow, setIsShow] = useState(false);
     const [isDone, setIsDone] = useState(word.isDone);
 
@@ -9,7 +10,37 @@ export default function Word({word}){
     }
 
     function toggleDone(){
-        setIsDone(!isDone);
+        //setIsDone(!isDone);
+        fetch(`http://localhost:3001/words/${word.id}`, {//두 번째 인자로 객체를 넣는다. 객체는 요청의 옵션들을 넣는다.
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json', //보내는 resorce의 타입
+            },
+            body : JSON.stringify({
+                ...word,
+                isDone: !isDone
+            })
+        }).then(res=>{
+            if(res.ok){
+                setIsDone(!isDone);
+            }
+        });
+    }
+    
+    function del(){
+        if(window.confirm('삭제 하시겠습니까?')){
+            fetch(`http://localhost:3001/words/${word.id}`, {
+                method: 'DELETE'
+            }).then(res=>{
+                if(res.ok){
+                    setWord({id:0});
+                }
+            });
+        }
+    }
+
+    if (word.id === 0){
+        return null;
     }
 
     return (
@@ -19,7 +50,7 @@ export default function Word({word}){
             <td>{isShow && word.kor}</td>
             <td>
                 <button onClick={toggleShow}>뜻 {isShow?'숨기기':'보기'}</button>
-                <button className='btn_del'>삭제</button>
+                <button onClick={del} className='btn_del'>삭제</button>
             </td>
         </tr>
     );
